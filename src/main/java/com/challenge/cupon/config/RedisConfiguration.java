9b8d365ce18edb30ecb0393c111585125d30dd7d
carlosmorera <carlos.morera@mail.escuelaing.edu.com>
2021-12-09 16:15:43 -0500

    Add integration with gcp

--- src/main/java/com/challenge/cupon/config/RedisConfiguration.java
+++ src/main/java/com/challenge/cupon/config/RedisConfiguration.java
@@ -3,6 +3,7 @@ package com.challenge.cupon.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -11,7 +12,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfiguration {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("10.2.15.35", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
